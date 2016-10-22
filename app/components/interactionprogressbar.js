04f34b3704954a18d081bec4f55bdd5643af1443
Henry Ecker <hecker@umass.edu>
2016-10-22 18:16:55 -0400

    Progress Bars Now Functioning

--- app/components/interactionprogressbar.js
+++ app/components/interactionprogressbar.js
@@ -1,19 +1,23 @@
 import React from "react"
+import { ProgressBar } from 'react-bootstrap';
 
+//Props percent barType children minutesLeft
 export default class InteractionProgressBar extends React.Component{
   render(){
-    return (
-          <div>
-          <h2 className="progess-header"> {this.props.children} </h2>
-          <div className="progress">
-              <div className={"progress-bar"+ this.props.barType} role="progressbar" style={{width:this.props.percent+"%"}} aria-valuenow={this.props.percent} aria-valuemin="0" aria-valuemax="100">
-                  <span style="color:black;">{this.props.percent}% Complete</span>
-              </div>
-              <div className="progress-bar" role="progressbar" style={"background-color:#A6A6A6;"+{width:(100-this.props.percent+"%")}}>
-                  <span style="color:black;">{this.minutesLeft} Minutes Left</span>
-              </div>
-          </div>
-          </div>
-    );
+      return (
+            <div>
+            <h2 className="progess-header"> {this.props.children} </h2>
+            <ProgressBar>
+              <ProgressBar now={this.props.percent} bsStyle={getsbStyle(this.props.percent)} label={this.props.percent+"%"}/>
+              <ProgressBar now={100-this.props.percent} bsStyle="info" label={this.props.minutesLeft+" Minutes Left"}/>
+            </ProgressBar>
+            </div>
+      );
   }
+}
+
+function getsbStyle(percent){
+  if(percent<=35) return "danger";
+  if(percent<=70) return "warning";
+  else return "success";
 }
--- app/components/interactionspanel.js
+++ app/components/interactionspanel.js
@@ -3,29 +3,16 @@ import InteractionProgressBar from "./InteractionProgressBar"
 
 export default class InteractionsPanel extends React.Component{
   render(){
+    function getPercent(minutesDone, totalMinutes){
+      return Math.floor((minutesDone/totalMinutes)*100);
+    }
     return(
           <div className="panel this-week-container">
               <h1 className="column-header text-left">This Week</h1>
               <div className="row">
-                  <InteractionProgressBar percent="73" minutesLeft="40" barType="progress-bar-success"> RASC Hours</InteractionProgressBar>
-                  <h2 className="progess-header"> RASC Hours </h2>
-                  <div className="progress">
-                      <div className="progress-bar progress-bar-success" role="progressbar" aria-valuenow="73" aria-valuemin="0" aria-valuemax="100" style="width:73%">
-                          <span style="color:black;">73% Complete</span>
-                      </div>
-                      <div className="progress-bar" role="progressbar" style="width:27%;background-color:#A6A6A6;">
-                          <span style="color:black;">40 Minutes Left</span>
-                      </div>
-                  </div>
-                  <h2 className="progess-header"> Out Of RASC Hours </h2>
-                  <div className="progress">
-                      <div className="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width:20%">
-                          <span style="color:black;">20% Complete</span>
-                      </div>
-                      <div className="progress-bar" role="progressbar" style="width:80%;background-color:#A6A6A6;">
-                          <span style="color:black;">240 Minutes Left</span>
-                      </div>
-                  </div>
+                <InteractionProgressBar percent={getPercent(170,450)} minutesLeft={450-170}> Total Hours</InteractionProgressBar>
+                <InteractionProgressBar percent={getPercent(110,150)} minutesLeft={150-110}> RASC Hours</InteractionProgressBar>
+                <InteractionProgressBar percent={getPercent(60,240)} minutesLeft={240-60}> Out of RASC Hours</InteractionProgressBar>
               </div>
               <h3 className="column-subtitle text-left">RASC Interactions</h3>
               <div className="row">
@@ -46,7 +33,7 @@ export default class InteractionsPanel extends React.Component{
                               <td className="interaction-cell">1 hour 15 minutes</td>
                               <td className="interaction-cell">Career Exploration</td>
                           </tr>
-                          <tr  className="interaction-row" data-toggle="collapse" data-target="#demo1">
+                          <tr colSpan="2" className="interaction-row" data-toggle="collapse" data-target="#demo1">
                               <td className="hiddenRow">
                                   <div className="collapse" id="demo1">
                                       <table className="table interaction-table">
@@ -77,7 +64,7 @@ export default class InteractionsPanel extends React.Component{
                               <td className="interaction-cell">35 minutes</td>
                               <td className="interaction-cell">Stress Management</td>
                           </tr>
-                          <tr  className="interaction-row" data-toggle="collapse" data-target="#demo2">
+                          <tr colSpan="12" className="interaction-row" data-toggle="collapse" data-target="#demo2">
                               <td className="hiddenRow">
                                   <div className="collapse" id="demo2">
                                       <table className="table interaction-table">
